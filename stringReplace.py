9d4e1e97a3829bd10e71a42198214472b77f81b7
ankitad87 <ankitad87@gmail.com>
2020-12-10 09:58:55 +0530

    Update stringReplace.py

--- stringReplace.py
+++ stringReplace.py
@@ -3,9 +3,11 @@ s1 = input("Enter the main string: ")
 s2 = input("Enter occurance string: ")
 s3 = input("Enter the replacement: ")
 
+#method 1
 print(s1.replace(s2, s3))
 print(s1)
 
+#method 2
 a1 = s1.split()
 a2 = s2.split()
 a3 = s3.split()
