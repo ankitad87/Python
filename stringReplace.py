969833a9b2ffed5d7838b483a9a90316bd659464
ankitad87 <ankitad87@gmail.com>
2020-12-10 09:55:29 +0530

    Create stringReplace.py
    Replace occurrence of string 2 in string 1 by string 3

---
+++ stringReplace.py
@@ -0,0 +1,23 @@
+
+s1 = input("Enter the main string: ")
+s2 = input("Enter occurance string: ")
+s3 = input("Enter the replacement: ")
+
+print(s1.replace(s2, s3))
+print(s1)
+
+a1 = s1.split()
+a2 = s2.split()
+a3 = s3.split()
+print(a3)
+i = 0
+news = ""
+
+for word in a1:
+    if word == s2:
+        word = s3
+    i += 1
+    news += str(word) + " "
+
+print("New string is :")
+print(news)
